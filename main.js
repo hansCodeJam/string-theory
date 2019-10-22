135c7eeac295eecefc551bd5d94d8b589ff1c745
Hans Vos <hans.vos@codeimmersives.com>
2019-10-21 22:38:59 -0400

    update 22:38

--- main.js
+++ main.js
@@ -1,25 +1,85 @@
-function crazyCase() {
+function crazyCase(str) {
+  let newString = '';
+      for(i = 0; i < str.length; i++) {
+      newString += i % 2 == 1 ? str.charAt(i).toUpperCase() : str.charAt(i);
+    }
+  
+  return newString;
 }
 
-function ciEmailify() {
+function exclaim(str) {
+  let newString = '';
+
+  for(let i = 0; i < str.length; i++) {
+    if(str[i] === '?') {
+      newString = newString + '!';
+    } else {
+      newString = newString + str[i];
+    }
+  }
+  return newString;
 }
 
-function exclaim() {
+function ciEmailify(str) {
+  let newString = '';
+
+  for(let i = 0; i < str.length; i++){
+    if(str[i] === ' ') {
+      newString = newString + '.';
+    } else {
+      newString = newString + str[i];
+    }
+  }
+  return `${newString.toLowerCase()}@codeimmersives.com`;
+  
 }
 
-function reverse() {
+function reverse(str) {
+  let splitStr = str.split("");
+  let reverseArray =  splitStr.reverse();
+  let joinArray = reverseArray.join("");
+
+  return joinArray;
 }
 
-function crazyCase2ReturnOfCrazyCase() {
+function crazyCase2ReturnOfCrazyCase(str) {
+  let stringNoSpace = str.split(' ').join('');
+  let newString = '';
+
+    for(let i = 0; i < stringNoSpace.length; i++) {
+    newString += i % 2 == 1 ? stringNoSpace.charAt(i).toUpperCase() : stringNoSpace.charAt(i);
+  }
+  
+  return newString;
 }
 
-function titleCase() {
+function titleCase(str) {
+  let newStringToLowerCaseSplit = str.toLowerCase().split(' ');
+  let newString = '';
+
+  for(let i = 0; i < newStringToLowerCaseSplit.length; i++) {
+    newString = newStringToLowerCaseSplit[0].toUpperCase() + newStringToLowerCaseSplit.substr(1);
+  }
+
+  return newString.join(' ');
+}
+// function toUpper(str) {
+//   return str
+//       .toLowerCase()
+//       .split(' ')
+//       .map(function(word) {
+//           console.log("First capital letter: "+word[0]);
+//           console.log("remain letters: "+ word.substr(1));
+//           return word[0].toUpperCase() + word.substr(1);
+//       })
+//       .join(' ');
+//    }
+function onlyVowels(str) {
+
 }
 
-function onlyVowels() {
-}
+function crazyCase3SonOfCrazyCase(str) {
 
-function crazyCase3SonOfCrazyCase() {
 }
 
 
